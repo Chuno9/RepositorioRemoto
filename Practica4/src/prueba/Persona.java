f654c86be2762cb1912b92300bece9155e70bf39
Alumno tarde <Alumno tarde@EQUIPO05>
2025-03-28 17:55:32 +0100

    Rama

---
+++ Practica4/src/prueba/Persona.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class Persona {
+String nombre;
+int edad;
+public Persona(String nombre, int edad) {
+	this.nombre = nombre;
+	this.edad = edad;
+}
+@Override
+public String toString() {
+	return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+}
+
+
+}
